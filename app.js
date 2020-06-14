99ff8b72bc26046cfc11b2098882add4e0a74bda
rutik wankhade <rutikwankhade2@gmail.com>
2020-06-14 23:45:53 +0530

    implemented fetch api

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+let input=document.querySelector('.categories');
 
+async function getApis(){
+    let category=input.value;
+
+     fetch(`https://api.publicapis.org/entries?category=${category}&https=true`)
+    
+    .then(apis=>{
+        data=apis.json();
+        return data;})
+        .then(data=>{console.log(data)})
+     
+    
+    
+}
+
+document.querySelector('.btn').addEventListener('click',getApis );
