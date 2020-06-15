76b75e45402cdc5049f3759a9919d098d557ba56
rutik wankhade <rutikwankhade2@gmail.com>
2020-06-15 13:45:03 +0530

    display apis

--- app.js
+++ app.js
@@ -8,10 +8,37 @@ async function getApis(){
     .then(apis=>{
         data=apis.json();
         return data;})
-        .then(data=>{console.log(data)})
+        .then(data=>{
+            console.log(data);
+            let count=data.count;
+
+            let entries=data.entries;
+            console.log(entries);
+            entries.forEach(el => {
+            displayApi(el);
+
+        
+            });
+            
+        })
      
     
     
 }
 
-document.querySelector('.btn').addEventListener('click',getApis );
+document.querySelector('.showBtn').addEventListener('click',getApis );
+let apiList=document.querySelector('.apis');
+
+const displayApi =(el)=>{
+
+    let markup=` 
+    <div class="apibox card">
+       <div class="color"></div>
+       <h4>${el.API}</h4>
+       <p>Description</p>
+       <p class="float-left">auth</p><button class="btn link float-right">View</button>
+    </div>
+ `;
+
+apiList.insertAdjacentHTML('beforeend',markup)
+}
