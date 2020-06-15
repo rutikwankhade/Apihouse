aa087176533bf96fbc3be28416a94315eb01bc66
rutik wankhade <rutikwankhade2@gmail.com>
2020-06-15 22:09:51 +0530

    restructured folders

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let input=document.querySelector('.categories');
+let count=document.querySelector('.count');
+let apiList=document.querySelector('.apis');
+
+document.querySelector('.showBtn').addEventListener('click',getApis );
 
 async function getApis(){
-    apiList.innerHTML="";
-    let category=input.value;
+      apiList.innerHTML="";
+      let category=input.value;
 
-     fetch(`https://api.publicapis.org/entries?category=${category}&https=true`)
+      fetch(`https://api.publicapis.org/entries?category=${category}&https=true`)
     
-    .then(apis=>{
-        data=apis.json();
-        return data;})
-        .then(data=>{
+      .then(apis=>{
+             data=apis.json();
+             return data;}
+            )
+       .then(data=>{
             console.log(data);
             count.textContent=`${data.count} Apis found`;
 
@@ -17,31 +22,28 @@ async function getApis(){
             console.log(entries);
 
             entries.forEach(el => {
-            if(el.Auth=="")
-            el.Auth='No Auth';
-                displayApi(el);
-
-        
+              if(el.Auth=="")
+                el.Auth='No Auth';
+                
+              displayApi(el);
             });
             
-        })
-     
-    
-    
-}
-let count=document.querySelector('.count');
+            })
 
-document.querySelector('.showBtn').addEventListener('click',getApis );
-let apiList=document.querySelector('.apis');
+        .catch(error =>alert(error))    
+          }
+          
+          
+    const displayApi =(el)=>{
 
-const displayApi =(el)=>{
-
-    let markup=` 
-    <div class="apibox card">
-       <h4 class="mt-4">${el.API}</h4>
-       <p class="blue">${el.Description}</p>
-       <p class="float-left">Auth Type : ${el.Auth}</p><button class="btn link float-right">View</button>
-    </div>
- `;
-apiList.insertAdjacentHTML('beforeend',markup)
+        let markup=` 
+        <div class="apibox card">
+            <h4 class="mt-4">${el.API}</h4>
+            <p class="blue">${el.Description}</p>
+            <p class="float-left">Auth Type : ${el.Auth}</p>
+            <a href=${el.Link} class="btn link float-right" >View</a>
+        </div>
+        `;
+        
+        apiList.insertAdjacentHTML('beforeend',markup)
 }
